9a12a368690235abee437da1485cde9836d2da00
Tharaka Madhusanka <tharakalearn@gmail.com>
2022-12-06 11:51:36 +0530

    Completed Backend Task 1

---
+++ WatiIo_API/WatiIo_API/Controllers/SumController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WatiIo_API.Models;
+
+namespace WatiIo_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumController : ControllerBase
+    {
+        [Route("add")]
+        [HttpPost]
+        public async Task<int> Sum(NumbersModel numbersModel)
+        {
+            return numbersModel.Number1 + numbersModel.Number2;
+;       }
+    }
+}
---
+++ WatiIo_API/WatiIo_API/Models/NumbersModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WatiIo_API.Models
+{
+    public class NumbersModel
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+    }
+}
