6cd327f57dd98aea2da36914e121e43e1ab5084c
Tharaka Madhusanka <tharakalearn@gmail.com>
2022-12-06 12:29:08 +0530

    FE Changes

---
+++ WatiIo_API/WatiIo_API/Database/DatabaseConnect.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace WatiIo_API.Database
+{
+    public class DatabaseConnect
+    {
+        public void AddToDatabase(int number)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sql = null;
+            SqlDataReader dataReader;
+            connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password";
+            sql = "Your SQL Statement Here , like Select * from product";
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                dataReader = command.ExecuteReader();
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
